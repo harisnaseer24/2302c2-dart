6b6553d2b10926ea88a284467b04a66fb7688807
harisnaseer24 <harisnaseer258@gmail.com>
2024-12-17 09:17:43 +0500

    basic of dart

---
+++ lib/intro.dart
@@ -0,0 +1,119 @@
+import 'dart:io';
+
+void main(){
+  // print("Hello World..!");
+  // print("Hello World..!");
+  // print("Hello World..!");
+  // print("Hello World..!");
+  // stdout.write("Bye");
+  // stdout.write("Bye");
+  // stdout.write("Bye");
+  // stdout.write("Bye");
+
+  // String name="Haris";
+  // print("My name is $name");
+  // print("My name is ${name.toUpperCase()}");
+
+//   print("Enter your name...!");
+//   String name = (stdin.readLineSync()!);//String "8"->convert ->int
+// print("My name is ${name.toUpperCase()}");
+
+// int age=78;
+// String book="The Envious";
+// bool? isVerified=false;
+
+// print("Enter your age...!");
+// int userAge= int.parse(stdin.readLineSync()!);
+// // conditional statement
+// if (userAge >= 50 && userAge <=100) {
+
+//   print("You need to take care of yourself eat healthy & excercise");
+// }
+// else if(userAge <=15 && userAge >4)  {
+//     print("Respect elders and listen their orders for right path.");
+// }
+
+//  else  {
+//     print("You need to work hard");
+// }
+
+// switch
+// var number = int.parse(stdin.readLineSync()!);
+// switch (number) {
+//   case 676876:
+//     print("Hi");
+//     break;
+//   default:
+//   print("Bye");
+// }
+
+
+
+//loops
+
+// for (when no of iterations are known)
+// 0-9=>10
+// int count =0;
+// for (int i = 0; i < 10; i++)//10
+//  {
+
+// for(int j=56; j<=72; j++)//17
+// {
+
+// // print("a");
+// for(int k=2; k<6;k++)
+// {
+//     count++;
+// print("a");
+// }
+// }
+  
+// }
+// print(count);
+
+// a. 17    b.70
+// c.27    d.170
+
+// while (when no of iterations are not known)
+
+// int i=10;
+// while(i<=5){
+//   print('a');
+  
+//   i++;
+// }
+//do while (when one iteration is must and the no of total iterations are not known)
+
+
+// int i=10;
+// do{
+//   print('a');
+  
+//   i++;
+// }while(i<=5);
+
+// task
+// print
+// stdout.write()
+/*
+
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+
+*/
+
+
+
+
+
+
+}
